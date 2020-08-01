ce4a1878ea51e34cde2327a7c0d16b0a6d459b06
Tom Ramsden <ramsdentom@gmail.com>
2020-08-01 01:17:44 +0100

    Implement gorilla mux

--- main.go
+++ main.go
@@ -2,50 +2,19 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-type server struct{}
-
-func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-
-	switch r.Method {
-	case "GET":
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte(`{"message": "got"}`))
-	case "POST":
-		w.WriteHeader(http.StatusCreated)
-		w.Write([]byte(`{"message": "posted"}`))
-	case "PUT":
-		w.WriteHeader(http.StatusAccepted)
-		w.Write([]byte(`{"message": "put"}`))
-	case "DELETE":
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte(`{"message": "deleted"}`))
-	default:
-		w.WriteHeader(http.StatusNotFound)
-		w.Write([]byte(`{"message": "404"}`))
-	}
+func index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello, World")
 }
 
 func main() {
-	// s := &server{}
-	// http.Handle("/", s)
-	// log.Fatal(http.ListenAndServe(":8080", nil))
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", index)
 
-	a := 4
-	b := &a
-
-	fmt.Printf("Address of var a: %p\n", b)
-	fmt.Printf("Value of var a: %v\n", *b)
-
-	*b = 2
-	fmt.Printf("Address of var a: %p\n", b)
-	fmt.Printf("Value of var a: %v\n", *b)
-
-	a = 1
-	fmt.Printf("Address of var a: %p\n", b)
-	fmt.Printf("Value of var a: %v\n", *b)
-
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
