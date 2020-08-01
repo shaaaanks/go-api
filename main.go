2c5e732cc30ab634163d5fab8159aa5d82bad362
Tom Ramsden <ramsdentom@gmail.com>
2020-08-01 01:22:51 +0100

    Add events struct, slice, and getevents function

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -8,13 +9,34 @@ import (
 	"github.com/gorilla/mux"
 )
 
+type event struct {
+	ID          string `json:"ID"`
+	Title       string `json:"Title"`
+	Description string `json:"Description"`
+}
+
+type allEvents []event
+
+var events = allEvents{
+	{
+		ID:          "1",
+		Title:       "Introduction to Golang",
+		Description: "Learn the Go",
+	},
+}
+
 func index(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Hello, World")
+}
+
+func getEvents(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(events)
 }
 
 func main() {
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", index)
+	router.HandleFunc("/events", getEvents)
 
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
